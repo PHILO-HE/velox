d61d6f9eb4b189b6e60ddc6fd806cb356b7ff9f8
PHILO-HE <feilong.he@intel.com>
2024-04-15 18:07:04 +0800

    Fix comment

--- velox/functions/sparksql/String.cpp
+++ velox/functions/sparksql/String.cpp
@@ -103,222 +103,266 @@ class Length : public exec::VectorFunction {
   }
 };
 
-void doApply(
-    const SelectivityVector& rows,
-    std::vector<VectorPtr>& args,
-    exec::EvalCtx& context,
-    const std::optional<std::string>& separator,
-    FlatVector<StringView>& flatResult) {
-  std::vector<column_index_t> argMapping;
-  std::vector<std::string> constantStrings;
-  auto numArgs = args.size();
-  argMapping.reserve(numArgs - 1);
-  // Save intermediate result for consecutive constant string args.
-  // They are concatenated in advance.
-  constantStrings.reserve(numArgs - 1);
-  // For each array arg, save rawSizes, rawOffsets, indices, and elements
-  // BaseBector.
-  std::vector<const vector_size_t*> rawSizesVector;
-  std::vector<const vector_size_t*> rawOffsetsVector;
-  std::vector<const vector_size_t*> indicesVector;
-  std::vector<DecodedVector> decodedVectors;
-
-  for (auto i = 1; i < numArgs; ++i) {
-    if (args[i] && args[i]->typeKind() == TypeKind::ARRAY) {
-      exec::LocalDecodedVector arrayHolder(context, *args[i], rows);
-      auto& arrayDecoded = *arrayHolder.get();
-      auto baseArray = arrayDecoded.base()->as<ArrayVector>();
-      rawSizesVector.push_back(baseArray->rawSizes());
-      rawOffsetsVector.push_back(baseArray->rawOffsets());
-      indicesVector.push_back(arrayDecoded.indices());
-      auto elements = baseArray->elements();
-      exec::LocalSelectivityVector nestedRows(context, elements->size());
-      nestedRows.get()->setAll();
-      exec::LocalDecodedVector elementsHolder(
-          context, *elements, *nestedRows.get());
-      auto& elementsDecoded = *elementsHolder.get();
-      decodedVectors.push_back(std::move(elementsDecoded));
-      continue;
-    }
-    // Handles string arg.
-    argMapping.push_back(i);
-    if (!separator.has_value()) {
-      // Cannot concat consecutive constant string args in advance.
-      constantStrings.push_back("");
-      continue;
-    }
-    if (args[i] && args[i]->as<ConstantVector<StringView>>() &&
-        !args[i]->as<ConstantVector<StringView>>()->isNullAt(0)) {
-      std::ostringstream out;
-      out << args[i]->as<ConstantVector<StringView>>()->valueAt(0);
-      column_index_t j = i + 1;
-      // Concat constant string args in advance.
-      for (; j < numArgs; ++j) {
-        if (!args[j] || args[j]->typeKind() == TypeKind::ARRAY ||
-            !args[j]->as<ConstantVector<StringView>>() ||
-            args[j]->as<ConstantVector<StringView>>()->isNullAt(0)) {
-          break;
-        }
-        out << separator.value()
-            << args[j]->as<ConstantVector<StringView>>()->valueAt(0);
-      }
-      constantStrings.emplace_back(out.str());
-      i = j - 1;
-    } else {
-      constantStrings.push_back("");
-    }
-  }
-
-  // Number of string columns after combined consecutive constant ones.
-  auto numStringCols = constantStrings.size();
-  // For column string arg decoding.
-  std::vector<exec::LocalDecodedVector> decodedStringArgs;
-  decodedStringArgs.reserve(numStringCols);
-  for (auto i = 0; i < numStringCols; ++i) {
-    if (constantStrings[i].empty()) {
-      auto index = argMapping[i];
-      decodedStringArgs.emplace_back(context, *args[index], rows);
-    }
-  }
-  exec::LocalDecodedVector separatorDecoded(context);
-  if (!separator.has_value()) {
-    separatorDecoded = exec::LocalDecodedVector(context, *args[0], rows);
-  }
-
-  // Calculate the total number of bytes in the result.
-  size_t totalResultBytes = 0;
-  rows.applyToSelected([&](auto row) {
-    int32_t allElements = 0;
-    // Array arg.
-    for (int i = 0; i < rawSizesVector.size(); i++) {
-      auto size = rawSizesVector[i][indicesVector[i][row]];
-      auto offset = rawOffsetsVector[i][indicesVector[i][row]];
-      for (int j = 0; j < size; ++j) {
-        if (!decodedVectors[i].isNullAt(offset + j)) {
-          auto element = decodedVectors[i].valueAt<StringView>(offset + j);
-          if (!element.empty()) {
-            allElements++;
-            totalResultBytes += element.size();
-          }
-        }
-      }
-    }
-
-    // String arg.
-    auto it = decodedStringArgs.begin();
-    for (int i = 0; i < numStringCols; i++) {
-      auto value = constantStrings[i].empty()
-          ? (*it++)->valueAt<StringView>(row)
-          : StringView(constantStrings[i]);
-      if (!value.empty()) {
-        allElements++;
-        totalResultBytes += value.size();
-      }
-    }
-
-    int32_t separatorSize = separator.has_value()
-        ? separator.value().size()
-        : separatorDecoded->valueAt<StringView>(row).size();
-
-    if (allElements > 1 && separatorSize > 0) {
-      totalResultBytes += (allElements - 1) * separatorSize;
-    }
-  });
-
-  // Allocate a string buffer.
-  auto rawBuffer = flatResult.getRawStringBufferWithSpace(totalResultBytes);
-  size_t bufferOffset = 0;
-  rows.applyToSelected([&](int row) {
-    const char* start = rawBuffer + bufferOffset;
-    size_t combinedSize = 0;
-    auto isFirst = true;
-    // For array arg.
-    int32_t i = 0;
-    // For string arg.
-    int32_t j = 0;
-    auto it = decodedStringArgs.begin();
-
-    auto copyToBuffer = [&](StringView value, StringView separator) {
-      if (value.empty()) {
-        return;
-      }
-      if (isFirst) {
-        isFirst = false;
-      } else {
-        // Add separator before the current value.
-        if (!separator.empty()) {
-          memcpy(rawBuffer + bufferOffset, separator.data(), separator.size());
-          bufferOffset += separator.size();
-          combinedSize += separator.size();
-        }
-      }
-      memcpy(rawBuffer + bufferOffset, value.data(), value.size());
-      combinedSize += value.size();
-      bufferOffset += value.size();
-    };
-
-    for (auto itArgs = args.begin() + 1; itArgs != args.end(); ++itArgs) {
-      if ((*itArgs)->typeKind() == TypeKind::ARRAY) {
-        auto size = rawSizesVector[i][indicesVector[i][row]];
-        auto offset = rawOffsetsVector[i][indicesVector[i][row]];
-        for (int k = 0; k < size; ++k) {
-          if (!decodedVectors[i].isNullAt(offset + k)) {
-            auto element = decodedVectors[i].valueAt<StringView>(offset + k);
-            copyToBuffer(
-                element,
-                separator.has_value()
-                    ? StringView(separator.value())
-                    : separatorDecoded->valueAt<StringView>(row));
-          }
-        }
-        i++;
-        continue;
-      }
-      if (j >= numStringCols) {
-        continue;
-      }
-      StringView value;
-      if (constantStrings[j].empty()) {
-        value = (*it++)->valueAt<StringView>(row);
-      } else {
-        value = StringView(constantStrings[j]);
-      }
-      copyToBuffer(
-          value,
-          separator.has_value() ? StringView(separator.value())
-                                : separatorDecoded->valueAt<StringView>(row));
-      j++;
-    }
-    flatResult.setNoCopy(row, StringView(start, combinedSize));
-  });
-}
-
 class ConcatWs : public exec::VectorFunction {
  public:
   explicit ConcatWs(const std::optional<std::string>& separator)
       : separator_(separator) {}
 
+  // Calculate the total number of bytes in the result.
+  size_t calculateTotalResultBytes(
+      const SelectivityVector& rows,
+      exec::EvalCtx& context,
+      std::vector<exec::LocalDecodedVector>& decodedArrays,
+      const std::vector<std::string>& constantStrings,
+      const std::vector<exec::LocalDecodedVector>& decodedStringArgs,
+      const exec::LocalDecodedVector& decodedSeparator) const {
+    size_t totalResultBytes = 0;
+    rows.applyToSelected([&](auto row) {
+      int32_t allElements = 0;
+      // Calculate size for array args.
+      for (int i = 0; i < decodedArrays.size(); i++) {
+        DecodedVector& arrayDecoded = *decodedArrays[i].get();
+        auto baseArray = arrayDecoded.base()->as<ArrayVector>();
+        auto rawSizes = baseArray->rawSizes();
+        auto rawOffsets = baseArray->rawOffsets();
+        auto indices = arrayDecoded.indices();
+        auto elements = baseArray->elements();
+        exec::LocalSelectivityVector nestedRows(context, elements->size());
+        nestedRows.get()->setAll();
+        exec::LocalDecodedVector elementsHolder(
+            context, *elements, *nestedRows.get());
+        auto& elementsDecoded = *elementsHolder.get();
+
+        auto size = rawSizes[indices[row]];
+        auto offset = rawOffsets[indices[row]];
+        for (int j = 0; j < size; ++j) {
+          if (!elementsDecoded.isNullAt(offset + j)) {
+            auto element = elementsDecoded.valueAt<StringView>(offset + j);
+            if (!element.empty()) {
+              allElements++;
+              totalResultBytes += element.size();
+            }
+          }
+        }
+      }
+
+      // Calculate size for string arg.
+      auto it = decodedStringArgs.begin();
+      for (int i = 0; i < constantStrings.size(); i++) {
+        auto value = constantStrings[i].empty()
+            ? (*it++)->valueAt<StringView>(row)
+            : StringView(constantStrings[i]);
+        if (!value.empty()) {
+          allElements++;
+          totalResultBytes += value.size();
+        }
+      }
+
+      int32_t separatorSize = separator_.has_value()
+          ? separator_.value().size()
+          : decodedSeparator->valueAt<StringView>(row).size();
+
+      if (allElements > 1 && separatorSize > 0) {
+        totalResultBytes += (allElements - 1) * separatorSize;
+      }
+    });
+    return totalResultBytes;
+  }
+
+  void initVectors(
+      const SelectivityVector& rows,
+      const std::vector<VectorPtr>& args,
+      const exec::EvalCtx& context,
+      std::vector<exec::LocalDecodedVector>& decodedArrays,
+      std::vector<column_index_t>& argMapping,
+      std::vector<std::string>& constantStrings,
+      std::vector<exec::LocalDecodedVector>& decodedStringArgs) const {
+    for (auto i = 1; i < args.size(); ++i) {
+      if (args[i] && args[i]->typeKind() == TypeKind::ARRAY) {
+        decodedArrays.emplace_back(context, *args[i], rows);
+        continue;
+      }
+      // Handles string arg.
+      argMapping.push_back(i);
+      if (!separator_.has_value()) {
+        // Cannot concat consecutive constant string args in advance.
+        constantStrings.push_back("");
+        continue;
+      }
+      if (args[i] && args[i]->as<ConstantVector<StringView>>() &&
+          !args[i]->as<ConstantVector<StringView>>()->isNullAt(0)) {
+        std::ostringstream out;
+        out << args[i]->as<ConstantVector<StringView>>()->valueAt(0);
+        column_index_t j = i + 1;
+        // Concat constant string args in advance.
+        for (; j < args.size(); ++j) {
+          if (!args[j] || args[j]->typeKind() == TypeKind::ARRAY ||
+              !args[j]->as<ConstantVector<StringView>>() ||
+              args[j]->as<ConstantVector<StringView>>()->isNullAt(0)) {
+            break;
+          }
+          out << separator_.value()
+              << args[j]->as<ConstantVector<StringView>>()->valueAt(0);
+        }
+        constantStrings.emplace_back(out.str());
+        i = j - 1;
+      } else {
+        constantStrings.push_back("");
+      }
+    }
+
+    // Number of string columns after combined consecutive constant ones.
+    auto numStringCols = constantStrings.size();
+    for (auto i = 0; i < numStringCols; ++i) {
+      if (constantStrings[i].empty()) {
+        auto index = argMapping[i];
+        decodedStringArgs.emplace_back(context, *args[index], rows);
+      }
+    }
+  }
+
+  // ConcatWs implementation. It concatenates the arguments with the separator.
+  // Mixed using of VARCHAR & ARRAY<VARCHAR> is considered. If separator is
+  // constant, concatenate consecutive constant string args in advance. Then,
+  // concatenate the intemediate result with neighboring array args or
+  // non-constant string args.
+  void doApply(
+      const SelectivityVector& rows,
+      std::vector<VectorPtr>& args,
+      exec::EvalCtx& context,
+      FlatVector<StringView>& flatResult) const {
+    std::vector<column_index_t> argMapping;
+    std::vector<std::string> constantStrings;
+    auto numArgs = args.size();
+    argMapping.reserve(numArgs - 1);
+    // Save intermediate result for consecutive constant string args.
+    // They will be concatenated in advance.
+    constantStrings.reserve(numArgs - 1);
+    std::vector<exec::LocalDecodedVector> decodedArrays;
+    decodedArrays.reserve(numArgs - 1);
+    // For column string arg decoding.
+    std::vector<exec::LocalDecodedVector> decodedStringArgs;
+    decodedStringArgs.reserve(numArgs);
+
+    initVectors(
+        rows,
+        args,
+        context,
+        decodedArrays,
+        argMapping,
+        constantStrings,
+        decodedStringArgs);
+    exec::LocalDecodedVector decodedSeparator(context);
+    if (!separator_.has_value()) {
+      decodedSeparator = exec::LocalDecodedVector(context, *args[0], rows);
+    }
+
+    auto totalResultBytes = calculateTotalResultBytes(
+        rows,
+        context,
+        decodedArrays,
+        constantStrings,
+        decodedStringArgs,
+        decodedSeparator);
+
+    // Allocate a string buffer.
+    auto rawBuffer = flatResult.getRawStringBufferWithSpace(totalResultBytes);
+    rows.applyToSelected([&](int row) {
+      const char* start = rawBuffer;
+      auto isFirst = true;
+      // For array arg.
+      int32_t i = 0;
+      // For string arg.
+      int32_t j = 0;
+      auto it = decodedStringArgs.begin();
+
+      auto copyToBuffer = [&](StringView value, StringView separator) {
+        if (value.empty()) {
+          return;
+        }
+        if (isFirst) {
+          isFirst = false;
+        } else {
+          // Add separator before the current value.
+          if (!separator.empty()) {
+            memcpy(rawBuffer, separator.data(), separator.size());
+            rawBuffer += separator.size();
+          }
+        }
+        memcpy(rawBuffer, value.data(), value.size());
+        rawBuffer += value.size();
+      };
+
+      for (auto itArgs = args.begin() + 1; itArgs != args.end(); ++itArgs) {
+        if ((*itArgs)->typeKind() == TypeKind::ARRAY) {
+          DecodedVector& arrayDecoded = *decodedArrays[i].get();
+          auto baseArray = arrayDecoded.base()->as<ArrayVector>();
+          auto rawSizes = baseArray->rawSizes();
+          auto rawOffsets = baseArray->rawOffsets();
+          auto indices = arrayDecoded.indices();
+          auto elements = baseArray->elements();
+          exec::LocalSelectivityVector nestedRows(context, elements->size());
+          nestedRows.get()->setAll();
+          exec::LocalDecodedVector elementsHolder(
+              context, *elements, *nestedRows.get());
+          auto& elementsDecoded = *elementsHolder.get();
+
+          auto size = rawSizes[indices[row]];
+          auto offset = rawOffsets[indices[row]];
+          for (int k = 0; k < size; ++k) {
+            if (!elementsDecoded.isNullAt(offset + k)) {
+              auto element = elementsDecoded.valueAt<StringView>(offset + k);
+              copyToBuffer(
+                  element,
+                  separator_.has_value()
+                      ? StringView(separator_.value())
+                      : decodedSeparator->valueAt<StringView>(row));
+            }
+          }
+          i++;
+          continue;
+        }
+        if (j >= constantStrings.size()) {
+          continue;
+        }
+        StringView value;
+        if (constantStrings[j].empty()) {
+          value = (*it++)->valueAt<StringView>(row);
+        } else {
+          value = StringView(constantStrings[j]);
+        }
+        copyToBuffer(
+            value,
+            separator_.has_value()
+                ? StringView(separator_.value())
+                : decodedSeparator->valueAt<StringView>(row));
+        j++;
+      }
+      flatResult.setNoCopy(row, StringView(start, rawBuffer - start));
+    });
+  }
+
   void apply(
-      const SelectivityVector& selected,
+      const SelectivityVector& rows,
       std::vector<VectorPtr>& args,
       const TypePtr& /* outputType */,
       exec::EvalCtx& context,
       VectorPtr& result) const override {
-    context.ensureWritable(selected, VARCHAR(), result);
+    context.ensureWritable(rows, VARCHAR(), result);
     auto flatResult = result->asFlatVector<StringView>();
     auto numArgs = args.size();
     // If separator is NULL, result is NULL.
     if (args[0]->isNullAt(0)) {
-      selected.applyToSelected([&](int row) { result->setNull(row, true); });
+      rows.applyToSelected([&](int row) { result->setNull(row, true); });
       return;
     }
     // If only separator (not a NULL) is provided, result is an empty string.
     if (numArgs == 1) {
-      selected.applyToSelected(
+      rows.applyToSelected(
           [&](int row) { flatResult->setNoCopy(row, StringView("")); });
       return;
     }
-    doApply(selected, args, context, separator_, *flatResult);
+    doApply(rows, args, context, *flatResult);
   }
 
  private:
@@ -386,11 +430,12 @@ std::shared_ptr<exec::VectorFunction> makeConcatWs(
     const std::vector<exec::VectorFunctionArg>& inputArgs,
     const core::QueryConfig& /*config*/) {
   auto numArgs = inputArgs.size();
-  VELOX_USER_CHECK(
-      numArgs >= 1,
+  VELOX_USER_CHECK_GE(
+      numArgs,
+      1,
       "concat_ws requires one arguments at least, but got {}.",
       numArgs);
-  for (auto& arg : inputArgs) {
+  for (const auto& arg : inputArgs) {
     VELOX_USER_CHECK(
         arg.type->isVarchar() ||
             (arg.type->isArray() &&
