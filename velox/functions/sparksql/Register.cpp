7fb985af2e0cbec590f1a2f476946b214b848e31
PHILO-HE <feilong.he@intel.com>
2024-04-15 10:44:31 +0800

    Initial commit

--- velox/functions/sparksql/Register.cpp
+++ velox/functions/sparksql/Register.cpp
@@ -324,6 +324,8 @@ void registerFunctions(const std::string& prefix) {
   registerFunction<YearFunction, int32_t, Date>({prefix + "year"});
   registerFunction<WeekFunction, int32_t, Timestamp>({prefix + "week_of_year"});
   registerFunction<WeekFunction, int32_t, Date>({prefix + "week_of_year"});
+  registerFunction<YearOfWeekSparkFunction, int32_t, Date>(
+      {prefix + "spark_year_of_week"});
 
   registerFunction<ToUtcTimestampFunction, Timestamp, Timestamp, Varchar>(
       {prefix + "to_utc_timestamp"});
